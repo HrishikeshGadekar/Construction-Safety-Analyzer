867bf8e355f256ec9f2f708e1f4fb860ea220e68
Hrishikesh <55317394+HrishikeshGadekar@users.noreply.github.com>
2021-09-16 14:37:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,31 +14,33 @@ def home():
 @app.route('/predict',methods=['POST','GET'])
 def predict():
 
-    vectorizer = TfidfVectorizer(min_df= 2, sublinear_tf=True, norm='l2', ngram_range=(1, 3))
-    #final_features = vectorizer.fit_transform(request.form.values()).toarray()
-    
-#     int_features = [float(x) for x in request.form.values()]
-#     final_features = [np.array(int_features)]
     prediction = model.predict(request.form.values())
     
     # Output Confidence scores
     probability_class_GO = model.predict_proba(request.form.values())[:, 0]
     probability_class_UA = model.predict_proba(request.form.values())[:, 1]
     probability_class_UC = model.predict_proba(request.form.values())[:, 2]
-
     
-    if prediction==0:
+
+    if probability_class_GO[0]==0.08765182261021813 or probability_class_UA[0]==0.1355507998162925 or probability_class_UC[0]==0.7767973775734893:
         return render_template('index.html',
-                               prediction_text='{}: This is a Good Observation!\n         The Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(prediction, probability_class_GO, probability_class_UA,probability_class_UC),
-                               
+                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                               prediction_text='---> This is an Invalid Input. Please try again.',
+                               )
+    elif prediction==0:
+        return render_template('index.html',
+                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                               prediction_text='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
                                )
     elif prediction==1:
         return render_template('index.html',
-                               prediction_text='{}: This is an Unsafe Act !!\n            The Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(prediction, probability_class_GO, probability_class_UA,probability_class_UC),
+                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                               prediction_text='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0])
                                )
     else:
         return render_template('index.html',
-                               prediction_text='{}: This is an Unsafe Condition !!\n      The Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(prediction, probability_class_GO, probability_class_UA,probability_class_UC),
+                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                               prediction_text='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0])
                               )
 
 if __name__ == "__main__":
