bcd7897606867ee6d966771045da3482eb560481
Hrishikesh <55317394+HrishikeshGadekar@users.noreply.github.com>
2021-10-08 17:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,49 +37,118 @@ def predict():
     probability_class_UA2 = model2.predict_proba(request.form.values())[:, 1]
     probability_class_UC2 = model2.predict_proba(request.form.values())[:, 2]
     
-
-    if probability_class_GO[0]==0.08765182261021813 or probability_class_UA[0]==0.1355507998162925 or probability_class_UC[0]==0.7767973775734893:
-        return render_template('index.html',
-                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
-                               prediction_text='---> This is an Invalid Input. Please try again.',
-                               )
-    elif prediction==0:
-        return render_template('index.html',
-                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
-                               prediction_text='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
-                               )
-    elif prediction==1:
-        return render_template('index.html',
-                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
-                               prediction_text='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
-                               )
-    elif prediction==2:
-        return render_template('index.html',
-                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
-                               prediction_text='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
-                              )
     
-    if probability_class_GO2[0]==0.08765182261021813 or probability_class_UA2[0]==0.1355507998162925 or probability_class_UC2[0]==0.7767973775734893:
-        return render_template('index.html',
-                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
-                               prediction_text='---> This is an Invalid Input. Please try again.',
-                               prediction_text2='---> This is an Invalid Input. Please try again.',
-                               )
-    elif prediction2==0:
-        return render_template('index.html',
-                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
-                               prediction_text2='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
-                               )
-    elif prediction2==1:
-        return render_template('index.html',
-                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
-                               prediction_text2='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
-                               )
-    elif prediction2==2:
-        return render_template('index.html',
-                               inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
-                               prediction_text2='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
-                               )
+    # P00
+    if probability_class_GO[0]==0.08765182261021813 or probability_class_UA[0]==0.1355507998162925 or probability_class_UC[0]==0.7767973775734893:
+        if probability_class_GO2[0]==0.08765182261021813 or probability_class_UA2[0]==0.1355507998162925 or probability_class_UC2[0]==0.7767973775734893:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Invalid Input. Please try again.',
+                                   prediction_text2='---> This is an Invalid Input. Please try again.',
+                                   )
+        elif prediction2==0:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Invalid Input. Please try again.',
+                                   prediction_text2='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+        elif prediction2==1:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Invalid Input. Please try again.',
+                                   prediction_text2='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+        elif prediction2==2:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Invalid Input. Please try again.',
+                                   prediction_text2='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+        
+    # P0    
+    elif prediction==0:
+        if probability_class_GO2[0]==0.08765182261021813 or probability_class_UA2[0]==0.1355507998162925 or probability_class_UC2[0]==0.7767973775734893:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is an Invalid Input. Please try again.',
+                                       )
+        elif prediction2==0:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+        elif prediction2==1:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+        elif prediction2==2:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+
+    
+    # P1
+    elif prediction==1:
+        if probability_class_GO2[0]==0.08765182261021813 or probability_class_UA2[0]==0.1355507998162925 or probability_class_UC2[0]==0.7767973775734893:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is an Invalid Input. Please try again.',
+                                       )
+        elif prediction2==0:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+        elif prediction2==1:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+        elif prediction2==2:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+
+    
+    # P2
+    elif prediction==2:
+        if probability_class_GO2[0]==0.08765182261021813 or probability_class_UA2[0]==0.1355507998162925 or probability_class_UC2[0]==0.7767973775734893:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is an Invalid Input. Please try again.',
+                                       )
+        elif prediction2==0:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is a Good Observation!\n The Prediction Confidence for each class are-\n GO: {},\n UA: {},\n UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+        elif prediction2==1:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is an Unsafe Act !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+        elif prediction2==2:
+            return render_template('index.html',
+                                   inp='Input: {}'.format(request.form.to_dict(flat=True)['Safety Observation']),
+                                   prediction_text='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO[0], probability_class_UA[0],probability_class_UC[0]),
+                                   prediction_text2='---> This is an Unsafe Condition !!\nThe Prediction Confidence for each class are- \n GO: {}, UA: {}, UC: {}'.format(probability_class_GO2[0], probability_class_UA2[0],probability_class_UC2[0])
+                                   )
+        
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
